08683a6b9ee95503c903f86c2aeb4f2b006357b9
allan <allan.nava@ies-italia.it>
2024-08-28 14:12:35 +0200

    added launch for vscode

--- lib/onesignal.go
+++ lib/onesignal.go
@@ -3,19 +3,20 @@ package lib
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/francoispqt/onelog"
 )
 
 // https://documentation.onesignal.com/reference/create-notification#sms-content
 
 type onesignalCfg struct {
-	AppID       string `json:"app_id"`
-    RestAppKey   string `json:"rest_api_key"`
-	Log         bool   `json:"log"`
+	AppID      string `json:"app_id"`
+	RestAppKey string `json:"rest_api_key"`
+	Log        bool   `json:"log"`
 }
 
 type onesignalMessenger struct {
-	cfg    onesignalCfg
+	cfg onesignalCfg
 
 	logger *onelog.Logger
 }
@@ -24,13 +25,11 @@ func (o onesignalMessenger) Name() string {
 	return "onesignal"
 }
 
-
 // Push sends the sms through onesignal API.
 func (p onesignalMessenger) Push(msg Message) error {
 
-    return nil
+	return nil
 }
-
 
 func (p onesignalMessenger) Flush() error {
 	return nil
@@ -40,10 +39,9 @@ func (p onesignalMessenger) Close() error {
 	return nil
 }
 
-
 // NewOneSignal creates new instance of pinpoint
 func NewOneSignal(cfg []byte, l *onelog.Logger) (Messenger, error) {
-    var c onesignalCfg
+	var c onesignalCfg
 	if err := json.Unmarshal(cfg, &c); err != nil {
 		return nil, err
 	}
@@ -51,8 +49,8 @@ func NewOneSignal(cfg []byte, l *onelog.Logger) (Messenger, error) {
 	if c.AppID == "" {
 		return nil, fmt.Errorf("invalid app_id")
 	}
-    //
-    return onesignalMessenger{
+	//
+	return onesignalMessenger{
 		cfg:    c,
 		logger: l,
 	}, nil
